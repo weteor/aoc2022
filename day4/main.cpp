56945f09096532ec68c86fd935097d216dcb946a
weteor <github@wetreg.fastmail.net>
2022-12-04 23:44:08 +0100

    remove unnecessary strings

--- day4/main.cpp
+++ day4/main.cpp
@@ -16,23 +16,17 @@ int main(void)
 
     while(getline(inputFile, line))
     {
-        size_t splitPos = line.find(",");
-        first = line.substr(0,splitPos);
-        second = line.substr(splitPos+1);
-
-        f1 = stoi(first.substr(0,first.find("-",0)));
-        f2 = stoi(first.substr(first.find("-")+1));
-        s1 = stoi(second.substr(0,second.find("-",0)));
-        s2 = stoi(second.substr(second.find("-")+1));
+        f1 = stoi(line.substr(                0, line.find("-")  ) );
+        f2 = stoi(line.substr(line.find("-") +1, line.find(",")  ) );
+        s1 = stoi(line.substr(line.find(",") +1 ,line.rfind("-") ) );
+        s2 = stoi(line.substr(line.rfind("-")+1                  ) );
          
-        if( ((f1<=s1) && (f2>=s2)) || 
-            ((s1<=f1) && (s2>=f2)) )
+        if( ((f1<=s1) && (f2>=s2)) || ((s1<=f1) && (s2>=f2)) )
         {
             containedPairs++;
         }
 
-        if( (f2 >= s1) && 
-            (s2 >= f1) )
+        if( (f2 >= s1) && (s2 >= f1) )
         {
             overlapPairs++;
         }
